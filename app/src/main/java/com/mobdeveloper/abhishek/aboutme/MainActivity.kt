66d1d204dcd993b2fa1fd054a81c52b8860ab375
abhishek <abhishekbhardwaj5638@gmail.com>
2019-02-12 14:48:29 -0500

    Applied Data Binding on Views

--- app/src/main/java/com/mobdeveloper/abhishek/aboutme/MainActivity.kt
+++ app/src/main/java/com/mobdeveloper/abhishek/aboutme/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.mobdeveloper.abhishek.aboutme
 
+import android.content.Context
+import android.databinding.DataBindingComponent
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.mobdeveloper.abhishek.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
+        binding.done.setOnClickListener{
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View){
+
+
+        binding.apply {
+            newNickName.text = editNickName.text
+            editNickName.visibility = View.GONE
+            view.visibility = View.GONE
+            newNickName.visibility = View.VISIBLE
+        }
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
+
     }
 }
